1ed33dde6c1afe39596aa54d512b86cd05a4e63b
4forces <62040770+4forces@users.noreply.github.com>
2022-01-19 13:53:55 +0800

    Done up to vid17 OOP Methods and Members

---
+++ csharp-101/vid16-19_OOP/vid17_methods&numbers/transaction.cs
@@ -0,0 +1,15 @@
+public class Transaction
+{
+    // Properties
+    public decimal  Amount { get; }
+    public DateTime Date { get; }
+    public string Notes { get; }
+
+    // Constructor
+    public Transaction  (decimal amount, DateTime date, string note)
+    {
+        this.Amount = amount;
+        this.Date = date;
+        this.Notes = note;
+    }
+}
